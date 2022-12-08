f4681d8ead5222201796cdffba8a99b0c7488c5d
arnor-nolen <andrei.gorbulin@rightware.com>
2022-12-08 14:16:16 +0200

    Added hidden files to Telescope

--- .config/nvim/lua/arnor/remap.lua
+++ .config/nvim/lua/arnor/remap.lua
@@ -1,8 +1,7 @@
 local nnoremap = require('arnor.keymap').nnoremap
 
 nnoremap("<leader>gf", "<cmd>Telescope git_files<CR>")
-nnoremap("<leader>ff", "<cmd>Telescope find_files<CR>")
-nnoremap("<leader>fb", "<cmd>Telescope file_browser<CR>")
+nnoremap("<leader>ff", "<cmd>Telescope find_files hidden=true<CR>")
 nnoremap("<leader>dg", "<cmd>Telescope diagnostics<CR>")
 nnoremap("<leader>gs", "<cmd>Telescope git_status<CR>")
 nnoremap("<leader>ds", "<cmd>Telescope lsp_document_symbols<CR>")
