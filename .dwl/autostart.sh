ab82708c03a6a375517e0bcd0863f8c1a943e020
Arnor Nolen <andrey1gorbulin@gmail.com>
2023-03-04 11:20:58 +0200

    Removed keyboard layouts

--- .dwl/autostart.sh
+++ .dwl/autostart.sh
@@ -6,8 +6,6 @@ dunst &
 pulsemixer --id sink-1 --set-volume 75 --id sink-3 --set-volume 75
 # Disk mounter
 udiskie &
-# Set keyboard layouts
-setxkbmap -layout us,ru,fi -option grp:win_space_toggle
 # Wayland bar
 yambar &
 # Wallpaper
