092630eb6737c90129aa790dfe4f7e18b277b3e1
arnor-nolen <andrei.gorbulin@rightware.com>
2023-09-06 16:44:14 +0300

    run-dwl-nvidia

--- .scripts/run-dwl-nvidia.sh
+++ .scripts/run-dwl-nvidia.sh
@@ -2,6 +2,8 @@
 
 # Hardware cursors not yet working on wlroots
 export WLR_NO_HARDWARE_CURSORS=1
+# Set WLRoots renderer to Vulkan to avoid flickering
+export WLR_RENDERER=vulkan
 # General wayland environment variables
 export XDG_SESSION_TYPE=wayland
 export QT_QPA_PLATFORM=wayland
@@ -9,5 +11,10 @@ export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
 # Firefox wayland environment variable
 export MOZ_ENABLE_WAYLAND=1
 export MOZ_USE_XINPUT2=1
+# OpenGL Variables
+export GBM_BACKEND=nvidia-drm
+export __GL_GSYNC_ALLOWED=0
+export __GL_VRR_ALLOWED=0
+export __GLX_VENDOR_LIBRARY_NAME=nvidia
 
 dwl
