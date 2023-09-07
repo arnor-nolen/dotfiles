d4f36863b646b3f57baf39c5ba4be077cba8aec4
Arnor Nolen <andrey1gorbulin@gmail.com>
2023-09-07 20:09:00 +0300

    Games

---
+++ .scripts/remove-scaling.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+wlr-randr --output DP-2 --scale 1 && wlr-randr --output HDMI-A-1 --pos 2560,0
---
+++ .scripts/restore-scaling.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+wlr-randr --output DP-2 --scale 1.25 && wlr-randr --output HDMI-A-1 --pos 2048,0
