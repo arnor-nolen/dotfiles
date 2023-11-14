7daf5e3f56ab90bec83bb8a1b1f1a802e0efa201
Arnor Nolen <andrey1gorbulin@gmail.com>
2023-11-14 18:12:49 +0200

    Don't crash when file does not exist

--- .scripts/clangd-qnx710.sh
+++ .scripts/clangd-qnx710.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-source /opt/sdk/qnx710/qnxsdp-env.sh
-clangd --query-driver=/opt/sdk/qnx710/host/linux/x86_64/usr/bin/*g++
+if [ -f /opt/sdk/qnx710/qnxsdp-env.sh ]
+then
+    source /opt/sdk/qnx710/qnxsdp-env.sh
+    clangd --query-driver=/opt/sdk/qnx710/host/linux/x86_64/usr/bin/*g++
+else
+    clangd
+fi
