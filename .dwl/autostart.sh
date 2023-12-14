c978869f9d77c2be06bc089aaf78cd7b035e8d22
Arnor Nolen <andrey1gorbulin@gmail.com>
2023-12-14 23:31:37 +0200

    No more waiting POG

--- .dwl/autostart.sh
+++ .dwl/autostart.sh
@@ -19,9 +19,8 @@ dwlb -ipc &
 # Discord
 discord --enable-features=UseOzonePlatform --ozone-platform=wayland &
 # Set volume to 75 for all sinks, 100 for inputs
-sleep 2 # Delay so sinks can initialize properly.
-pulsemixer --list-sinks | awk '{print substr($3, 0, length($3) - 1)}' | xargs -I {} pulsemixer --id {} --set-volume 75
-pulsemixer --list-sources | awk '{print substr($3, 0, length($3) - 1)}' | xargs -I {} pulsemixer --id {} --set-volume 100
+sleep 2 && pulsemixer --list-sinks | awk '{print substr($3, 0, length($3) - 1)}' | xargs -I {} pulsemixer --id {} --set-volume 75 && kill -37 $(pidof status-text) & 
+sleep 2 && pulsemixer --list-sources | awk '{print substr($3, 0, length($3) - 1)}' | xargs -I {} pulsemixer --id {} --set-volume 100 && kill -37 $(pidof status-text) &
 # Status bar
 killall status-text
 status-text | dwlb -status-stdin all &
