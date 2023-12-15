81d75fd0a3a2d3ef05e98893e13baa6f7c8eb142
Arnor Nolen <andrey1gorbulin@gmail.com>
2023-12-15 19:24:49 +0200

    Added hyprland nvidia config

---
+++ .scripts/run-hyprland-nvidia.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Set WLRoots renderer to Vulkan to avoid flickering
+export WLR_RENDERER=vulkan
+# OpenGL Variables
+export GBM_BACKEND=nvidia-drm
+export __GL_GSYNC_ALLOWED=0
+export __GL_VRR_ALLOWED=0
+export __GLX_VENDOR_LIBRARY_NAME=nvidia
+
+/home/arnor/.scripts/run-hyprland.sh
