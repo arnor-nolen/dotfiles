0c484acc4c64e47f1fd8400dbbcf831523c35c4a
arnor-nolen <andrei.gorbulin@rightware.com>
2024-01-16 09:08:09 +0200

    Added compress alias

--- .dwl/autostart.sh
+++ .dwl/autostart.sh
@@ -19,8 +19,8 @@ dwlb -ipc &
 # Discord
 discord --enable-features=UseOzonePlatform --ozone-platform=wayland &
 # Set volume to 75 for all sinks, 100 for inputs
-sleep 2 && pulsemixer --list-sinks | awk '{print substr($3, 0, length($3) - 1)}' | xargs -I {} pulsemixer --id {} --set-volume 75 && kill -37 $(pidof status-text) & 
-sleep 2 && pulsemixer --list-sources | awk '{print substr($3, 0, length($3) - 1)}' | xargs -I {} pulsemixer --id {} --set-volume 100 && kill -37 $(pidof status-text) &
+sleep 3 && pulsemixer --list-sinks | awk '{print substr($3, 0, length($3) - 1)}' | xargs -I {} pulsemixer --id {} --set-volume 75 && kill -37 $(pidof status-text) & 
+sleep 3 && pulsemixer --list-sources | awk '{print substr($3, 0, length($3) - 1)}' | xargs -I {} pulsemixer --id {} --set-volume 100 && kill -37 $(pidof status-text) &
 # Status bar
 killall status-text
 status-text | dwlb -status-stdin all &
