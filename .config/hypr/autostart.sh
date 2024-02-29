3294e5e36a73d4c2ac2137e1101469cc46ae8ef9
Arnor Nolen <andrey1gorbulin@gmail.com>
2024-02-29 21:55:32 +0200

    Updated hyprland configs

--- .config/hypr/autostart.sh
+++ .config/hypr/autostart.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 # Notifications
 dunst &
-# Set volume to 50 for headphones and speakers
-pulsemixer --id sink-1 --set-volume 75 --id sink-3 --set-volume 75
 # Disk mounter
 udiskie &
-# Set keyboard layouts
-setxkbmap -layout us,ru,fi -option grp:win_space_toggle
 # Wayland bar
 waybar &
 # Wallpaper
 hyprpaper &
 # Foot terminal server
 foot --server &
-# Check pacman updates
-pacman -Sy &
+# Discord
+discord --enable-features=UseOzonePlatform --ozone-platform=wayland &
+# Set volume to 75 for all sinks, 100 for inputs
+pulsemixer --list-sinks | awk '{print substr($3, 0, length($3) - 1)}' | xargs -I {} pulsemixer --id {} --set-volume 75
+pulsemixer --list-sources | awk '{print substr($3, 0, length($3) - 1)}' | xargs -I {} pulsemixer --id {} --set-volume 100
