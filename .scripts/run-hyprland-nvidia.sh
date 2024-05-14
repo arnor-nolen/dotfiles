1c9f8cd90aff5fdf808d1e67e670d45513b27975
Arnor Nolen <andrey1gorbulin@gmail.com>
2024-05-14 23:05:43 +0300

    Removed unsupported Hyprland backend

--- .scripts/run-hyprland-nvidia.sh
+++ .scripts/run-hyprland-nvidia.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # Set WLRoots renderer to Vulkan to avoid flickering
-export WLR_RENDERER=vulkan
+# Vulkan backend is not supported by Hyprland.
+# export WLR_RENDERER=vulkan
 # OpenGL Variables
 export GBM_BACKEND=nvidia-drm
 export __GL_GSYNC_ALLOWED=1
