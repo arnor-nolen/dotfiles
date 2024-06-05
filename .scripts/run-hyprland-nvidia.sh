9cb389b0185cd77b74449668eb0667b57a7e4fdc
Arnor Nolen <andrey1gorbulin@gmail.com>
2024-06-05 20:34:02 +0300

    Linux 6.9 workaround

--- .scripts/run-hyprland-nvidia.sh
+++ .scripts/run-hyprland-nvidia.sh
@@ -3,6 +3,9 @@
 # Set WLRoots renderer to Vulkan to avoid flickering
 # Vulkan backend is not supported by Hyprland.
 # export WLR_RENDERER=vulkan
+# Required to workaround a bug introduced in Linux 6.9
+# https://github.com/hyprwm/Hyprland/issues/6273
+export WLR_DRM_DEVICES=/dev/dri/card1
 # OpenGL Variables
 export GBM_BACKEND=nvidia-drm
 export __GL_GSYNC_ALLOWED=1
