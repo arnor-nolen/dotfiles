27361f2d0db13b40e38633987b3f5ed3ce2b5815
Arnor Nolen <andrey1gorbulin@gmail.com>
2025-07-01 00:22:07 +0300

    Windows VM + performance tuning.

--- vm/windows10/windows10.sh
+++ vm/windows10/windows10.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 qemu-system-x86_64 \
-    -drive file=kde.img,format=raw,index=0,media=disk,if=virtio,cache=none,aio=native,cache.direct=on \
-    -nic user,model=virtio-net-pci \
+    -drive file=windows10.img,format=raw,index=0,media=disk,cache=none,aio=native,cache.direct=on \
     -m 8G \
     -enable-kvm \
     -smp 8,sockets=1,cores=4,threads=2 \
     -vga virtio \
     -display spice-app,gl=on \
-    -cpu host,topoext=on \
+    -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,topoext=on \
     -device virtio-serial-pci \
     -spice unix=on,addr=/tmp/vm_spice.socket,disable-ticketing=on \
     -device virtserialport,chardev=spicechannel0,name=com.redhat.spice.0 \
