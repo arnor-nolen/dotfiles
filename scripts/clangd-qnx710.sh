b9baf45209368f5e66eb885d8b74771587575f20
arnor-nolen <andrei.gorbulin@rightware.com>
2025-10-21 11:10:25 +0300

    QNX clangd fix.

--- scripts/clangd-qnx710.sh
+++ scripts/clangd-qnx710.sh
@@ -3,7 +3,8 @@
 if [ -f /opt/sdk/qnx710/qnxsdp-env.sh ]
 then
     source /opt/sdk/qnx710/qnxsdp-env.sh
-    clangd --query-driver=/opt/sdk/qnx710/host/linux/x86_64/usr/bin/*g++
+    # Newer clang versions broke QNX support.
+    /usr/lib/llvm18/bin/clangd --query-driver=/opt/sdk/qnx710/host/linux/x86_64/usr/bin/*g++
 else
     clangd
 fi
